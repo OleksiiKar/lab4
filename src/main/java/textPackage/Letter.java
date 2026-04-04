48b63955a3f0dbc2f21d2c98c0cf5098f1402693
OleksiiKar <oleksiykar@icloud.com>
2026-04-04 14:28:55 +0300

    Add function of puntuation and letter

--- src/main/java/textPackage/Letter.java
+++ src/main/java/textPackage/Letter.java
@@ -1,4 +1,17 @@
 package textPackage;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Letter {
+    @Getter @Setter
+    private char myChar;
+    public Letter(char chars){
+        this.myChar = chars;
+    }
+
+    @Override
+    public String toString(){
+        return String.valueOf(myChar);
+    }
 }
--- src/main/java/textPackage/Punctuation.java
+++ src/main/java/textPackage/Punctuation.java
@@ -1,4 +1,19 @@
 package textPackage;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
 public class Punctuation {
+    @Getter @Setter
+    private char myPunctuation;
+
+    public Punctuation(char myPunctuation){
+        this.myPunctuation = myPunctuation;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(myPunctuation);
+    }
 }
